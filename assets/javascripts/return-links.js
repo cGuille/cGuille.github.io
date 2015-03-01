dac5e8b7b3d24975a67cfeb4619c360ec2c6f15e
Guillaume Charmetant <cguille.dev@gmail.com>
2015-03-01 10:30:50 +0100

    Return link do not lead to external referrer

--- assets/javascripts/return-links.js
+++ assets/javascripts/return-links.js
@@ -1,7 +1,7 @@
 window.document.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
-  if (!window.history || !document.referrer) {
+  if (!window.history || !document.referrer.contains(document.location.host)) {
     return;
   }
   document.querySelectorAll('a.return-link')
