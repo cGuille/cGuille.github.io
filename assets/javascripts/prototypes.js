5af7d2ff5fae256037cb2a9d22d7ce3ffeddbb0a
Guillaume Charmetant <cguille.dev@gmail.com>
2015-03-01 10:30:33 +0100

    Add String.prototype.contains()

--- assets/javascripts/prototypes.js
+++ assets/javascripts/prototypes.js
@@ -7,6 +7,11 @@
   if (!HTMLCollection.prototype.toArray) {
     HTMLCollection.prototype.toArray = toArray;
   }
+  if (!String.prototype.contains) {
+    String.prototype.contains = function String_contains(searchString) {
+      return this.indexOf(searchString) !== -1;
+    };
+  }
 
   function toArray() {
     return Array.prototype.slice.call(this);
