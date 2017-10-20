87f3dbcf7bacdf98a28cdc97bf8440fdebfccd03
Guillaume Charmetant <cguille.dev@gmail.com>
2017-10-20 20:19:43 +0200

    add noopener / noreferrer to target="_blank" links

--- assets/javascripts/external-links-targets.js
+++ assets/javascripts/external-links-targets.js
@@ -7,6 +7,21 @@ window.document.addEventListener('DOMContentLoaded', function () {
             return linkElt.host !== document.location.host;
         })
         .forEach(function (externalLinkElt) {
+            var linkRelParts = [];
+
+            if (externalLinkElt.rel) {
+                linkRelParts.push(externalLinkElt.rel);
+            }
+
+            if (!/\bnoopener\b/.test(externalLinkElt.rel)) {
+                linkRelParts.push('noopener');
+            }
+
+            if (!/\bnoreferrer\b/.test(externalLinkElt.rel)) {
+                linkRelParts.push('noreferrer');
+            }
+
+            externalLinkElt.rel = linkRelParts.join(' ');
             externalLinkElt.target = '_blank';
         });
 });
